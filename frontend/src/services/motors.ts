c6b94a0477e7e4d015a9f1f3c94bf40d16260e93
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-05-14 15:56:21 +0200

    feat(frontend): Added function to get motor data

--- frontend/src/services/motors.ts
+++ frontend/src/services/motors.ts
@@ -1,6 +1,19 @@
 import api from "./api";
 
+// Get all motors
 export async function getMotors() {
   const res = await api.get("/motors/");
+  return res.data;
+}
+
+// Latest motor data
+export async function getLatestMotor(motorId: number) {
+  const res = await api.get(`/motors/${motorId}/telemetry/latest`);
+  return res.data;
+}
+
+// Historical motor data
+export async function getMotorHistory(motorId: number) {
+  const res = await api.get(`/motors/${motorId}/telemetry/history`);
   return res.data;
 }

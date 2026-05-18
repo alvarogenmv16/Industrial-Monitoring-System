ea98ac17ad49a90a5ef7b9d792ca864d54120272
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-05-18 15:34:15 +0200

    feat(frontend): Added anomalies and status

--- frontend/src/services/motors.ts
+++ frontend/src/services/motors.ts
@@ -1,5 +1,9 @@
 import api from "./api";
 
+// ===============
+// MOTORS
+// ===============
+
 // Get all motors
 export async function getMotors() {
   const res = await api.get("/motors/");
@@ -15,5 +19,37 @@ export async function getLatestMotor(motorId: number) {
 // Historical motor data
 export async function getMotorHistory(motorId: number) {
   const res = await api.get(`/motors/${motorId}/telemetry/history`);
+  return res.data;
+}
+
+// ===============
+// STATUS OVERVIEW
+// ===============
+
+export async function getMotorsStatusOverview() {
+  const res = await api.get("/motors/status/overview");
+  return res.data;
+}
+
+// ===============
+// ANOMALIES
+// ===============
+
+export async function getMotorAnomalies() {
+  const res = await api.get("/motors/anomalies");
+  return res.data;
+}
+
+export async function getMotorAnomaliesOverview(
+  start_time?: string,
+  end_time?: string
+) {
+  const res = await api.get("/motors/anomalies/overview", {
+    params: {
+      start_time,
+      end_time,
+    },
+  });
+
   return res.data;
 }
